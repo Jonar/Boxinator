4ace1460a217be54c6f4cf3df4c25e78e1a18832
Jon Widén <jon.widen@ericsson.com>
2020-04-22 00:39:29 +0200

    Box input validation

--- src/AddBox.js
+++ src/AddBox.js
@@ -1,37 +1,66 @@
 import React, { useState } from 'react';
 
+const matchName = '(\\p{L}+ ?)+(?<! )$'; //Unicode letters. Allow space between words but not at the end.
+    //TODO: consider using trim on input instead of lookahead regex to handle trailing space
+
 function AddBox({addDispatch}) {
     const initialState = {
-        receiver: 'Receiver name',
-        weight: 0,
+        receiver: '',
+        weight: '', //number, initially empty
         color: "#8DC891",
         country: 'Sweden'
     };
     const [box, setBox] = useState(initialState);
     function handleInput(event) {
-        const { name, value } = event.target;
+        const { name, value } = event.target; //name refers to form input field
         setBox({ ...box, [name]: value });
     }
     function handleSubmit(event){
+        event.preventDefault(); //avoid URL change
         addDispatch(box);
-        event.preventDefault();
+    }
+    function checkWeight(event){
+        const input = event.target;
+        if(input.valueAsNumber < 0) {
+            input.setCustomValidity('Negative weight is not permitted');
+        } else {
+            input.setCustomValidity(''); //value is fine - clear error message
+        }
     }
     return (
         <form onSubmit={handleSubmit}>
             <label htmlFor="receiver">Name</label>
-            <input type="text" id="receiver" name="receiver" value={box.receiver} onChange={handleInput}/>
+            <input type="text" id="receiver" name="receiver" required={true}
+                placeholder={'Receiver'} pattern={matchName} title={'Full name. Space can be used between names'}
+                value={box.receiver} onChange={handleInput}/>
+
             <label htmlFor="weight">Weight</label>
-            <input type="number" name="weight" id="weight" value={box.weight} onChange={handleInput}/>
+            <input type="number" name="weight" id="weight" required={true}
+                placeholder={'0 kg'} min={0} step={0.001}
+                onInput={checkWeight}
+                onInvalid={(event) => {
+                    const input = event.target;
+                    if (input.valueAsNumber < 0) {
+                        setBox({...box, weight: 0}); //default to 0
+                        setTimeout((input) => input.setCustomValidity(''), 2000, input); //error timeout
+                    }
+                }}
+                value={box.weight} onChange={handleInput}/>
+
             <label htmlFor="color">Box color</label>
-            <input type="color" name="color" id="color" value={box.color} onChange={handleInput}/> {/* TODO: Component*/}
+            <input type="color" name="color" id="color" required={true}
+                value={box.color} onChange={handleInput}/> {/* TODO: Component*/}
+
             <label htmlFor="country">Country</label>
-            <select name="country" id="country" value={box.country} onChange={handleInput}>
+            <select name="country" id="country" required={true}
+                value={box.country} onChange={handleInput}>
                 <option value="Sweden">Sweden</option>
                 <option value="China">China</option>
                 <option value="Brazil">Brazil</option>
                 <option value="Australia">Australia</option>
             </select>
-            <input type="submit"/>
+
+            <input type="submit" value="Save"/>
         </form>
     );
 }
